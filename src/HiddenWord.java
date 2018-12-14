9d4d803986dbcb011b9502137926ce3a815a8638
BT_1N3_12@bktech.local <BT_1N3_12@bktech.local>
2018-12-14 11:13:54 -0500

    Initial commit

---
+++ src/HiddenWord.java
@@ -0,0 +1,20 @@
+public class HiddenWord {
+    private String word;
+
+    public HiddenWord(String word) {
+        this.word = word;
+    }
+
+    public String getHint(String guess) {
+        String hint = "";
+        for (int i = 0; i < this.word.length(); i++) {
+            if (word.substring(i, i + 1).equals(guess.substring(i, i + 1)))
+                hint += this.word.substring(i, i + 1);
+            else if (word.indexOf(guess.substring(i, i + 1)) > -1)
+                hint += "+";
+            else
+                hint += "*";
+        }
+        return hint;
+    }
+}
